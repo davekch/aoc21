076e3fcc883afcfc9e35324390051da25dfe6619
davekch <dave-koch@web.de>
2021-12-16 23:00:12 +0100

    day 15 part 2 in python

--- day15/solver.py
+++ day15/solver.py
@@ -11,60 +11,64 @@ measure_time = utils.stopwatch()
 
 @measure_time
 def parse(raw_data):
-    return [list(map(int, line)) for line in raw_data.splitlines()]
-    # graph = {}
-    # for y, line in enumerate(raw_data.splitlines()):
-    #     for x, v in enumerate(line):
-    #         graph[(x, y)] = int(v)
-    # return graph
+    # return [list(map(int, line)) for line in raw_data.splitlines()]
+    graph = {}
+    for y, line in enumerate(raw_data.splitlines()):
+        for x, v in enumerate(line):
+            graph[(x, y)] = int(v)
+    return graph
 
 
 def dijkstra(start, dest, grid):
-    xdim, ydim = len(grid[0]), len(grid)
-    unvisited = set((x, y) for x in range(xdim) for y in range(ydim))
+    unvisited = set(grid.keys())
     visited = set()
-    paths = defaultdict(list)
     distances = {p: np.inf for p in unvisited}
     distances[start] = 0
     while unvisited:
         current_candidates = {p: d for p,d in distances.items() if p in unvisited}
         current = min(current_candidates, key=current_candidates.get)
         x, y = current
-        neighbors = {
-            p for p in {(x, min(y+1, ydim-1)), (x, max(y-1, 0)),
-                        (min(x+1, xdim-1), y), (max(x-1, 0), y)}
-            if p not in visited
-        }
+        neighbors = [(x, y+1), (x, y-1),
+                     (x+1, y), (x-1, y)]
         for n in neighbors:
-            distance = distances[current] + grid[n[1]][n[0]]
+            if n in visited or n not in grid:
+                continue
+            distance = distances[current] + grid[n]
             if distance < distances[n]:
                 distances[n] = distance
-                paths[n] = paths[current] + [n]
         unvisited.remove(current)
         visited.add(current)
-    return distances[dest], paths[dest]
-
-
-def dijkstra2(start, end, graph):
-    unvisited = set(graph.keys())
-    visited = set()
-    distances = {p: np.inf for p in univisited}
-    distances[start] = 0
-    # while unvisited:
+    return distances[dest]
 
 
 # PART 1
 @measure_time
 def solve1(data):
-    target = (len(data[0])-1, len(data)-1)
-    distance, path = dijkstra((0, 0), target, data)
-    return distance
+    target = max(data)
+    return dijkstra((0, 0), target, data)
 
+
+def wrap(a, b):
+    if a + b < 10:
+        return a+b
+    else:
+        return (a+b) % 10 + 1
 
 # PART 2
 @measure_time
 def solve2(data):
-    pass
+    xm, ym = max(data)
+    xdim, ydim = xm+1, ym+1
+    for (x, y) in list(data.keys()):
+        for i in range(5):
+            for j in range(5):
+                if (i, j) in [(0,0), (0, 3), (0, 4), (1, 4), (3, 0), (4, 0), (4, 1)]:
+                    continue
+                if i == 0 and j == 0:
+                    continue
+                data[(x + xdim*j, y + ydim*i)] = wrap(data[(x, y)], i+j)
+    target = max(data)
+    return dijkstra((0, 0), target, data)
 
 
 if __name__ == "__main__":
